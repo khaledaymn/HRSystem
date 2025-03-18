1baceda706b97eae9686ca0abe60402bb9c81992
khaledaymn <khaled654ayman0@gmail.com>
2025-03-18 02:52:50 +0200

    Create Project

---
+++ HRSystem/Filters/CustomSchemaFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace HRSystem.Filters
+{
+    public class CustomSchemaFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (schema?.Properties == null)
+            {
+                return;
+            }
+
+            foreach (var property in schema.Properties)
+            {
+                property.Value.Nullable = true;
+            }
+        }
+    }
+}
---
+++ HRSystem/Program.cs
@@ -0,0 +1,209 @@
+﻿#region Usings
+using HRSystem.Filters;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.Text;
+
+#endregion
+
+#region Configiration Services
+
+#region Web Aplication
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+#endregion
+
+
+#region API Configration
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+#endregion
+
+
+#region Connection String Configration
+
+#endregion
+
+
+#region identity Configration
+
+#endregion
+
+
+#region CORS Configration
+
+#endregion
+
+
+#region Authentication Configration
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+#endregion
+
+
+#region Get Section Configration
+
+#region JWT Configration
+
+#endregion
+
+#region Admin Configration
+
+#endregion
+
+#region Email Configration
+
+#endregion
+
+#endregion
+
+
+#region Dependency Injection Configration
+
+#endregion
+
+
+#region OpenAPI Configration
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+#endregion
+
+
+#region Swagger Configration
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "HR System API",
+        Description = "API documentation for HR System",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Khaled Ayman",
+            Email = "khaled654ayman0@gmail.com"
+        }
+        //License = new OpenApiLicense
+        //{
+        //    Name = "Not Found any license",
+        //    Url = new Uri("https://example.com/license")
+        //}
+    });
+
+    // JWT Authentication setup
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+
+    // Enable XML comments if available
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    option.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+    // Make schema properties optional if they have null values
+    option.SchemaFilter<CustomSchemaFilter>();
+});
+#endregion
+
+
+#region Build
+
+var app = builder.Build();
+
+#endregion
+
+#endregion
+
+#region Check Environment
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+#endregion
+
+#region Meddleware
+
+#region Swagger Meddleware
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    var prefix = string.IsNullOrEmpty(options.RoutePrefix) ? "." : "..";
+    options.SwaggerEndpoint($"{prefix}/swagger/v1/swagger.json", "HR System API v1");
+    options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+    options.DisplayRequestDuration();
+});
+#endregion
+
+#region Cors Meddelwere
+
+#endregion
+
+#region HTTPS Meddleware
+app.UseHttpsRedirection();
+#endregion
+
+#region Authentication and Authorization Meddelwere
+app.UseAuthentication();
+app.UseAuthorization();
+#endregion
+
+#region Endpoints
+app.MapControllers();
+
+app.Run();
+
+#endregion
+
+#endregion
